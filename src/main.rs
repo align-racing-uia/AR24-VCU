2d576bb54dcad00b5120bea18399cdd67086059e
Håvard Nygård <haavard.hn@gmail.com>
2024-06-08 00:06:09 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@
 mod fmt;
 
 use cortex_m::Peripherals;
-use embassy_sync::{blocking_mutex::raw::{NoopRawMutex, ThreadModeRawMutex}, mutex::Mutex};
+use embassy_sync::{blocking_mutex::{raw::{NoopRawMutex, ThreadModeRawMutex}, ThreadModeMutex}, mutex::Mutex};
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_stm32::{bind_interrupts, can::{self, filter::{ExtendedFilter, ExtendedFilterSlot, Filter, StandardFilter, StandardFilterSlot}, frame, Can, Instance}, exti, gpio::{AnyPin, Level, Output, Pin, Speed}, peripherals::{FDCAN1, FDCAN2}, time::mhz, Config, Peripheral};
+use embassy_stm32::{adc, bind_interrupts, can::{self, filter::{ExtendedFilter, ExtendedFilterSlot, Filter, StandardFilter, StandardFilterSlot}, frame, BufferedCan, Can, CanRx, CanTx, Instance, RxBuf, TxBuf}, exti, gpio::{AnyPin, Level, Output, Pin, Speed}, peripherals::{FDCAN1, FDCAN2}, time::mhz, Config, Peripheral};
 use embassy_time::{Instant, Timer};
 use fmt::info;
 
@@ -25,6 +25,13 @@ bind_interrupts!(struct Irqs2 {
     FDCAN2_IT1 => can::IT1InterruptHandler<FDCAN2>;
 });
 
+static MUT_APPS: Mutex<ThreadModeRawMutex, APPS> = Mutex::new(APPS::new());
+static MUT_VCU: Mutex<ThreadModeRawMutex, VCU> = Mutex::new(VCU::new());
+static MUT_INVERTER:  Mutex<ThreadModeRawMutex, Inverter> = Mutex::new(Inverter::new());
+static MUT_DRIVER_INTERFACE: Mutex<ThreadModeRawMutex, DriverInterface> = Mutex::new(DriverInterface::new());
+
+const INVERTER_NODE_ID: u8 = 30;
+const MAX_CURRENT: u16 = 230;
 
 enum InverterCommand {
     SetCurrent,
@@ -36,8 +43,6 @@ enum InverterCommand {
 }
 
 struct Inverter {
-    node_id: u8,
-    max_current: u16,
     dc_voltage: u16,
     erpm: u32,
     duty_cycle: u16,
@@ -51,10 +56,8 @@ struct Inverter {
 }
 
 impl Inverter {
-    fn new() -> Inverter {
+    const fn new() -> Inverter {
         Inverter {
-            node_id: 30,
-            max_current: 230,
             dc_voltage: 0,
             erpm: 0,
             duty_cycle: 0,
@@ -77,7 +80,7 @@ struct APPS {
 }
 
 impl APPS {
-    fn new() -> APPS {
+    const fn new() -> APPS {
         APPS { throttle1: 0, throttle2: 0, brake_pressure1: 0, brake_pressure2: 0 }
     }
 }
@@ -90,7 +93,7 @@ struct VCU {
 }
 
 impl VCU {
-    fn new() -> VCU {
+    const fn new() -> VCU {
         VCU { ready_to_drive: false, drive_enable: false, applied_throttle: 0, error_code: 0 }
     }
 }
@@ -100,39 +103,40 @@ struct DriverInterface {
 }
 
 impl DriverInterface {
-    fn new() -> DriverInterface {
+    const fn new() -> DriverInterface {
         DriverInterface { r2d: false }
     }
 }
 
-async fn drive_command<T>(command: InverterCommand, can: &mut Can<'_, T>, vcu: &mut VCU,  inverter: &mut Inverter, apps: &mut APPS, driver_interface: &mut DriverInterface)
+async fn drive_command<T>(command: InverterCommand, can: &mut CanTx<'_, T>)
 where
     T: Instance
 {
     match command {
         InverterCommand::SetCurrent => {
+            let apps = MUT_APPS.lock().await;
             let mut data = [0u8; 8];
-            let mut current = inverter.max_current * apps.throttle1 as u16 / 100;
+            let current = MAX_CURRENT * apps.throttle1 as u16 / 100;
             data[1] = current as u8;
-            data[0] = (current >> 8) as u8;
-            send_can_packet(can, 0x01, 30, &data).await;
-
+            data[0] = (current >> 8) as u8;    
+            send_can_packet(can, 0x01, 30, &data).await;   
         },
         InverterCommand::SetBrakeCurrent => {},
         InverterCommand::SetERPM => {},
         InverterCommand::SetPosition => {},
         InverterCommand::SetRelativeCurrent => {},
         InverterCommand::SetDriveEnable => {
+            let vcu = MUT_VCU.lock().await;
             let mut data = [0u8; 8];
             data[0] += vcu.drive_enable as u8;
-            send_can_packet(can, 0x0C, inverter.node_id, &data).await;
+            send_can_packet(can, 0x0C, INVERTER_NODE_ID, &data).await;
         },
     }
 
 
 }
 
-async fn send_can_packet<T>(can: &mut Can<'_, T>, packet_id: u16, node_id: u8, data: &[u8; 8])
+async fn send_can_packet<T>(can: &mut CanTx<'_, T>, packet_id: u16, node_id: u8, data: &[u8; 8])
 where
     T: Instance
 {
@@ -141,70 +145,86 @@ where
     can.write(&frame).await;
 }
 
-async fn read_drive_can<T>(can: &mut Can<'_, T>, vcu: &mut VCU,  inverter: &mut Inverter, apps: &mut APPS, driver_interface: &mut DriverInterface)
-where
-    T: Instance
+#[embassy_executor::task]
+async fn read_drive_can(mut can: CanRx<'static, FDCAN2>)
 {
-    match can.read().await {
-        Ok(envelope) => {
-            let frame = envelope.frame;
-            let data = frame.data();            
-            let (packet_id, node_id) = {
-                match frame.id() {
-                    embedded_can::Id::Standard(id) => {
-                        ((id.as_raw() >> 5) as u16, (id.as_raw() & 0x1F) as u8)
-                    },
-                    embedded_can::Id::Extended(id) => {
-                        ((id.as_raw() >> 8) as u16, (id.as_raw() & 0xFF) as u8)
-                    },
-                }
-            };
-            match node_id {
-                0x1E => { // ID = 30 Inverter
-                    match packet_id {
-                        0x20 => {
-                            inverter.erpm = (data[3] as u32) | (data[2] as u32) << 8  | (data[1] as u32) << 16 | (data[0] as u32) << 24; // lol
-                            inverter.duty_cycle = (data[5] as u16) | (data[4] as u16) << 8;
-                            inverter.dc_voltage = (data[7] as u16) | (data[6] as u16) << 8;
-                            
+    loop {
+        match can.read().await {
+            Ok(envelope) => {
+                let frame = envelope.frame;
+                let data = frame.data();        
+                let (packet_id, node_id) = {
+                    match frame.id() {
+                        embedded_can::Id::Standard(id) => {
+                            ((id.as_raw() >> 5) as u16, (id.as_raw() & 0x1F) as u8)
                         },
-                        0x21 => {
-                            inverter.ac_current = (data[1] as u16) | (data[0] as u16) << 8;
-                            inverter.dc_current = (data[3] as u16) | (data[2] as u16) << 8;
+                        embedded_can::Id::Extended(id) => {
+                            ((id.as_raw() >> 8) as u16, (id.as_raw() & 0xFF) as u8)
                         },
-                        0x22 => {
-                            inverter.inverter_temp = (data[1] as u16) | (data[0] as u16) << 8;
-                            inverter.motor_temp    = (data[3] as u16) | (data[2] as u16) << 8;
-                            inverter.fault_code    = data[5];
-                        },
-                        0x23 => {
-                            // No relevant messages
-                        },
-                        0x24 => {
-                            inverter.drive_enable_feedback = (data[4] >> 7) != 0;
-                        },
-                        _ => {
-                            info!("Unknown packet from inverter!")
+                    }
+                };
+                match node_id {
+                    0x1E => { // ID = 30 Inverter
+                        let mut inverter = MUT_INVERTER.lock().await;
+                        match packet_id {
+                            0x20 => {
+                                inverter.erpm = (data[3] as u32) | (data[2] as u32) << 8  | (data[1] as u32) << 16 | (data[0] as u32) << 24; // lol
+                                inverter.duty_cycle = (data[5] as u16) | (data[4] as u16) << 8;
+                                inverter.dc_voltage = (data[7] as u16) | (data[6] as u16) << 8;
+                                
+                            },
+                            0x21 => {
+                                inverter.ac_current = (data[1] as u16) | (data[0] as u16) << 8;
+                                inverter.dc_current = (data[3] as u16) | (data[2] as u16) << 8;
+                            },
+                            0x22 => {
+                                inverter.inverter_temp = (data[1] as u16) | (data[0] as u16) << 8;
+                                inverter.motor_temp    = (data[3] as u16) | (data[2] as u16) << 8;
+                                inverter.fault_code    = data[5];
+                            },
+                            0x23 => {
+                                // No relevant messages
+                            },
+                            0x24 => {
+                                inverter.drive_enable_feedback = (data[4] >> 7) != 0;
+                            },
+                            _ => {
+                                info!("Unknown packet from inverter!")
+                            }
                         }
+                    },
+                    0x0A => {
+                        let mut apps = MUT_APPS.lock().await;
+                        match packet_id {
+                            0x0A => {
+                                apps.throttle1 = data[0];
+                                apps.throttle2 = data[1];
+                                apps.brake_pressure1 = data[2];
+                                apps.brake_pressure2 = data[3];
+                            }
+                            _ => {}
+                        }
+                    },
+                    0x0E => {
+                        let mut driver_interface = MUT_DRIVER_INTERFACE.lock().await;
+                        match packet_id {  
+                            0x20 => {
+                                driver_interface.r2d = data[0] > 0;
+                            },
+                            _ => {}
+                        }
+                    },
+                    _ => {
+                        info!("Unknown sender! Checking against compatible basic IDs");
                     }
-                },
-                0x0E => {
-                    match packet_id {
-                        0x20 => {
-                            driver_interface.r2d = data[0] > 0;
-                        },
-                        _ => {}
-                    }
-                },
-                _ => {
-                    info!("Unknown sender! Checking against compatible basic IDs");
                 }
+            },
+            Err(_) => {
+                info!("Oh no")
             }
-        },
-        Err(_) => {
-            info!("Oh no")
         }
     }
+
 }
 
 #[embassy_executor::task]
@@ -251,49 +271,74 @@ async fn main(spawner: Spawner) {
     let mut buzzer = Output::new(p.PC14, Level::Low, Speed::Low);
     let mut extra = Output::new(p.PC15, Level::Low, Speed::Low);
     
-    let mut can2 = {
+    let can2 = {
         let mut can = can::CanConfigurator::new(p.FDCAN2, p.PB5, p.PB6, Irqs2);
         can.properties().set_extended_filter(ExtendedFilterSlot::_0, ExtendedFilter::accept_all_into_fifo0());
-        can.properties().set_extended_filter(ExtendedFilterSlot::_1, ExtendedFilter::accept_all_into_fifo1());
-        can.properties().set_standard_filter(StandardFilterSlot::_1, StandardFilter::accept_all_into_fifo1());
         can.config().set_non_iso_mode(false);
-        can.set_bitrate(1_000_000);
+        can.set_bitrate(500_000);
         can.into_normal_mode()
     
     };
+
     let led = Output::new(p.PB9, Level::High, Speed::Low);
     spawner.spawn(blinker(led)).unwrap();
 
-    let mut vcu = VCU::new();
-    let mut apps = APPS::new();
-    let mut inverter = Inverter::new();
-    let mut driver_interface = DriverInterface::new();
-
     let mut command_timestamp = Instant::now();
+    let (mut can2_tx, mut can2_rx, mut _can2_properties) = can2.split();
+
+    spawner.spawn(read_drive_can(can2_rx)).unwrap();
 
     loop {
-        // Update all states from can
-        read_drive_can(&mut can2, &mut vcu, &mut inverter, &mut apps, &mut driver_interface).await;
-
-        if driver_interface.r2d && inverter.dc_voltage > 350 && inverter.fault_code == 0 { // This has to be programmed muuuch better
-            vcu.ready_to_drive = true;
-        }
         
-        if apps.brake_pressure1 > 3 {
-            brakelight.set_high();
-        }else{
-            brakelight.set_low();
+        // APPS Section
+        let brake_pressure: u8;
+        { // Control brakelight
+            let apps = MUT_APPS.lock().await;
+            if apps.brake_pressure1 > 3 {
+                brakelight.set_high();
+            }else{
+                brakelight.set_low();
+            }
+            brake_pressure = apps.brake_pressure1; // As of now we only care about 1 brake sensor
         }
+
+        // Inverter section
+        let dc_voltage: u16; let dc_current: u16; let ac_current: u16;
+        let inverter_temp: u16; let motor_temp: u16; let erpm: u32;
+        {
+            let inverter = MUT_INVERTER.lock().await;
+            dc_voltage = inverter.dc_voltage;
+            dc_current = inverter.dc_current;
+            ac_current = inverter.ac_current;
+            inverter_temp = inverter.inverter_temp;
+            motor_temp = inverter.motor_temp;
+            erpm = inverter.erpm;
+
+
+        }
+
+        // Modify internal logic
+        let ready_to_drive: bool;
+        {
+            let vcu = MUT_VCU.lock().await;
+            ready_to_drive = vcu.ready_to_drive;
+
+        }
+
+
+
+        {   // Todo buzzer logic
+            
+
+        }
+
 
         if command_timestamp.elapsed().as_millis() >= 20 {
-            drive_command(InverterCommand::SetDriveEnable, &mut can2, &mut vcu, &mut inverter, &mut apps, &mut driver_interface).await;
-
-            if vcu.ready_to_drive {
-                drive_command(InverterCommand::SetCurrent,  &mut can2, &mut vcu, &mut inverter, &mut apps, &mut driver_interface).await;
-            }
+            drive_command(InverterCommand::SetDriveEnable, &mut can2_tx).await;
 
             command_timestamp = Instant::now();
         }
+        Timer::after_micros(10).await;
         
     }
 }
